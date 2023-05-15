7e8110ad9df27c1b99939d49db421c2f0710fec4
MaxPan <inator@ukr.net>
2023-05-15 16:33:00 +0300

    V1

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+class GameWord
+{
+private:
+	string _word;
+	string _wordFound;
+	int _letters;
+	int _lettersFound;
+public:
+
+};
+
+int main()
+{
+
+}
