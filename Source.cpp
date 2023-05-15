aa4c0272e0e226ea2e97e778f0c235a9def1b2f4
MaxPan <inator@ukr.net>
2023-05-15 20:26:45 +0300

    V2

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,7 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+
 
 using namespace std;
 
@@ -10,10 +13,139 @@ private:
 	int _letters;
 	int _lettersFound;
 public:
+	GameWord()
+	{
+		_word = "";
+		_wordFound = "";
+		_letters = 0;
+		_lettersFound = 0;
+	}
+	void setRandom()
+	{
+		ifstream file{ "words.txt" };
+		while (file.is_open())
+		{
+			for (int i = 0; i < rand() % 466427; i++)
+			{
+				getline(file, _word);
+			}
+			file.close();
+		}
+		_letters = _word.length();
+		_lettersFound = 0;
+		_wordFound.clear();
+		_wordFound = "";
+		for (int i = 0; i < _letters; i++)
+			_wordFound.append("_");
+	}
+};
 
+
+class Interface
+{
+public:
+	void loadScreen(int attemps)
+	{
+		switch (attemps)
+		{
+		case 0:
+			cout << "+---+\n";
+			cout << "|   |\n";
+			cout << "|   O\n";
+			cout << "|  /|\\\n";
+			cout << "|  / \\\n";
+			break;
+		case 1:
+			cout << "+---+\n";
+			cout << "|   |\n";
+			cout << "|   O\n";
+			cout << "|  /|\\\n";
+			cout << "|  /\n";
+			break;
+		case 2:
+			cout << "+---+\n";
+			cout << "|   |\n";
+			cout << "|   O\n";
+			cout << "|  /|\\\n";
+			cout << "|\n";
+			break;
+		case 3:
+			cout << "+---+\n";
+			cout << "|   |\n";
+			cout << "|   O\n";
+			cout << "|  /|\n";
+			cout << "|\n";
+			break;
+		case 4:
+			cout << "+---+\n";
+			cout << "|   |\n";
+			cout << "|   O\n";
+			cout << "|  /\n";
+			cout << "|\n";
+			break;
+		case 5:
+			cout << "+---+\n";
+			cout << "|   |\n";
+			cout << "|   O\n";
+			cout << "|\n";
+			cout << "|\n";
+			break;
+		case 6:
+			cout << "+---+\n";
+			cout << "|   |\n";
+			cout << "|\n";
+			cout << "|\n";
+			cout << "|\n";
+			break;
+		default:
+			break;
+		}
+	}
+};
+class Game
+{
+private:
+	Interface _gameInterface;
+	int _attemps;
+public:
+	Game()
+	{
+		_attemps = 0;
+	}
+	void playerTurn(GameWord word)
+	{
+		
+	}
+	void play()
+	{
+		srand(time(0));
+		while (true)
+		{
+			GameWord word;
+			playerTurn(word);
+			if (!_attemps)
+			{
+
+			}
+		}
+	}
 };
 
 int main()
 {
-
+	
+	string _word;
+	ifstream file{ "words.txt" };
+	int randomLine = rand() % 466427; //counting amount of the lines is taking too much time
+	cout << "Line: " << randomLine << "\n";
+	while (file.is_open())
+	{
+		for (int i = 0; i < rand() % 466427; i++)
+		{
+			getline(file, _word);
+		}
+		break;
+	}
+	file.close();
+	cout << "Word: " << _word << "\n";
 }
